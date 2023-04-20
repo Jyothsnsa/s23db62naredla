cb84cdd02c4181084f555cbbfb9a80686994fdf1
Jyothsnsa <123060113+Jyothsnsa@users.noreply.github.com>
2023-04-19 19:36:08 -0500

    changes done

--- controllers/books.js
+++ controllers/books.js
@@ -12,9 +12,17 @@ exports.books_list = async function(req, res) {
     }
 };
 // for a specific books.
-exports.books_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: books detail: ' + req.params.id);
-};
+exports.books_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await books.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle books create on POST.
 exports.books_create_post = async function(req, res) {
     console.log(req.body)
@@ -35,14 +43,45 @@ exports.books_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-// Handle books delete form on DELETE.
-exports.books_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
+   // Handle books update form on PUT.
+exports.books_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await books.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.books_type)
+            toUpdate.books_type = req.body.books_type;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
+// // Handle books delete form on DELETE.
+// exports.books_delete = function(req, res) {
+//  res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
+// };
+// Handle books delete on DELETE.
+exports.books_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await books.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
 // Handle books update form on PUT.
-exports.books_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
-};
+
 
 // VIEWS
 // Handle a show all view
@@ -56,3 +95,55 @@ exports.books_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   // Handle a show one view with id specified by query
+exports.books_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await books.findById( req.query.id)
+    res.render('booksdetail',
+    { title: 'books Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for creating a books.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.books_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('bookscreate', { title: 'books Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    //Handle building the view for updating a books.
+// query provides the id
+exports.books_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await books.findById(req.query.id)
+res.render('booksupdate', { title: 'books Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+  // Handle a delete one view with id from query
+exports.books_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await books.findById(req.query.id)
+    res.render('booksdelete', { title: 'books Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };  
--- routes/books.js
+++ routes/books.js
@@ -4,5 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', books_controlers.books_view_all_Page );
+/* GET create books page */
+router.get('/create', books_controlers.books_create_Page);
+/* GET create update page */
+router.get('/update', books_controlers.books_update_Page);
+/* GET delete books page */
+router.get('/delete', books_controlers.books_delete_Page);
 
+/* GET detail books page */
+router.get('/detail', books_controlers.books_view_one_Page);
 module.exports = router;
