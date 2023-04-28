acc065b433c8732ce5f4507735b757113d4afa5e
Jyothsnsa <123060113+Jyothsnsa@users.noreply.github.com>
2023-04-27 21:08:10 -0500

    new endpoint

--- routes/books.js
+++ routes/books.js
@@ -4,13 +4,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', books_controlers.books_view_all_Page );
-/* GET create books page */
-router.get('/create', books_controlers.books_create_Page);
-/* GET create update page */
-router.get('/update', books_controlers.books_update_Page);
-/* GET delete books page */
-router.get('/delete', books_controlers.books_delete_Page);
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}   
+/* GET create update page */
+router.get('/update',secured, books_controlers.books_update_Page);
 /* GET detail books page */
-router.get('/detail', books_controlers.books_view_one_Page);
+router.get('/detail',books_controlers.books_view_one_Page);
+
+/* GET create books page */
+router.get('/create',secured, books_controlers.books_create_Page);
+
+
+/* GET delete books page */
+router.get('/delete',secured, books_controlers.books_delete_Page);
+
 module.exports = router;
